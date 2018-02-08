37c9102cae2a90df4452b2f141f49e30939a935b
Amy Chew <amychew@amys-mbp.attlocal.net>
2018-02-07 17:17:05 -0800

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//document.write('&copy'+ new Date().getFullYear())
+
+
+//var currentYear = new Date().getFullYear();
+//document.getElementById("printed-year").innerHTML = currentYear;
+
+var currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
